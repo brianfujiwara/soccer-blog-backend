f3230b015161f19f5e3fc8953d3f581de6837dc9
brianobmalay <nikadas157@comcast.net>
2020-08-05 16:20:06 -0500

    my first commit

---
+++ src/main/java/com/firsttouch/soccerblogbackend/SoccerBlogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.firsttouch.soccerblogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerBlogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerBlogBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firsttouch/soccerblogbackend/SoccerBlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firsttouch.soccerblogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerBlogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
