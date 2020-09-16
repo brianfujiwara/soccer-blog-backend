f0227b8637d8fc3d299ab3947b368e811ca9b06c
brianobmalay <nikadas157@comcast.net>
2020-09-16 15:42:29 -0500

    security

--- src/main/java/com/firsttouch/soccerblogbackend/entity/User.java
+++ src/main/java/com/firsttouch/soccerblogbackend/entity/User.java
@@ -4,6 +4,11 @@ package com.firsttouch.soccerblogbackend.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name="users")
@@ -11,9 +16,34 @@ import javax.persistence.*;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String userName;
+    private Long id;
+
+    @NotBlank
+    @Size(max =20)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max =120)
     private String password;
 
-    private String roles;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(	name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public User() {
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
 }
